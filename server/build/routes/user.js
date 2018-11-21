56eaf96888f4c3e0a99443fb93ba7d7b540445ee
coolbeatz71 <sigmacool@gmail.com>
2018-11-21 09:19:14 +0200

    [Feature #162027526] Add auth controller for user to signUp

---
+++ server/build/routes/user.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _auth = require('../controllers/auth');
+
+var _auth2 = _interopRequireDefault(_auth);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var router = _express2.default.Router();
+
+/**
+ * TESTED
+ * route to sign-up the user to its account
+ * @method POST
+ */
+router.post('/signUp', _auth2.default.userSignUp);
+
+module.exports = router;
