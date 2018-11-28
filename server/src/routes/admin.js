b0a2e7890e43bc0bdba505c6d75d152682d23a4e
coolbeatz71 <sigmacool@gmail.com>
2018-11-28 18:39:46 +0200

    [Feature #162275527] Add the route to count parcel using DB

--- server/src/routes/admin.js
+++ server/src/routes/admin.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AdminCtrl from '../controllers/admin';
+import checkAuth from '../middleware/auth';
 
 const router = express.Router();
 
@@ -9,5 +10,12 @@ const router = express.Router();
  * @method POST
  */
 router.post('/login', AdminCtrl.adminSignIn);
+
+/**
+ * TESTED
+ * route to get the number of parcels delivery orders for all users
+ * @method GET
+ */
+router.get('/parcels/count', checkAuth, AdminCtrl.countParcels);
 
 module.exports = router;
