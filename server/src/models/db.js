e17d10eb90ff40968dff769d97148b8c2cb399c0
coolbeatz71 <sigmacool@gmail.com>
2018-12-02 13:11:15 +0200

    [Feature #162156069] Redeploy the rest-api

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -6,12 +6,23 @@ const envPath = path.resolve(__dirname, '../../../.env');
 
 dotenv.config({ path: envPath });
 
+// set dev or test environment
+const myEnv = process.env.NODE_ENV ? `${process.env.NODE_ENV.toUpperCase()}_` : '';
+
+const dbConfig = {
+  user: process.env[`${myEnv}DB_USERNAME`],
+  host: process.env[`${myEnv}DB_HOST`],
+  database: process.env[`${myEnv}DATABASE`],
+  password: process.env[`${myEnv}DB_PASSWORD`],
+  port: process.env[`${myEnv}DB_PORT`],
+};
+
+console.log(dbConfig);
+
 /**
  * Credit to charles odili https://github.com/chalu/pre-bc-workshops/
  */
-const pool = new Pool({
-  connectionString: 'postgres://zqovliavkubvcd:396486bb17d5f53e5a0309f27adfacac3a430caa7f9f60f265001304547b419c@ec2-50-19-249-121.compute-1.amazonaws.com:5432/d2f450emadmm6q',
-});
+const pool = new Pool(dbConfig);
 
 // create the DB connection
 const connect = async () => pool.connect();
