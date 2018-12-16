c36282d387e48216690b33a8f65b13e7812a9558
coolbeatz71 <sigmacool@gmail.com>
2018-12-16 19:21:35 +0200

    Revert "#162669533 Document the REST API with swagger"

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
-import swaggerUi from 'swagger-ui-express';
 import authRoutes from './auth';
 import userRoutes from './user';
 import parcelRoutes from './parcel';
 import adminRoutes from './admin';
 
-import swaggerDocument from '../api-docs-swagger.json';
-
-
 const router = express.Router();
 
-router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/parcels', parcelRoutes);
